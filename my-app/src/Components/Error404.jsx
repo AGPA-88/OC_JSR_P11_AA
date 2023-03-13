903d6858d02509dd203ccf80e06ff04142eadfa0
Antonio Alexandre <antonio.alexandre@marte.fr>
2023-03-13 16:22:13 +0100

    Error 404 page updated

--- my-app/src/Components/Error404.jsx
+++ my-app/src/Components/Error404.jsx
@@ -5,7 +5,7 @@ function Error404 () {
     return (
     <div className='Error404'>
         <img src={errorImg} alt="404 error" />
-        <h1>Oops! It looks like this page doesn’t exist.</h1>
+        <div className='error-text'>Oops! It looks like this page doesn’t exist.</div>
     <div className='hp_link'>
         <a href='/home'>Go back to home page</a>
     </div>
