6be584191afed5c2b86b331a3a53c89f1fe4d83c
Antonio Alexandre <antonio.alexandre@marte.fr>
2023-03-13 16:19:08 +0100

    Gallery updated

--- my-app/src/Components/HousePage/Gallery/index.jsx
+++ my-app/src/Components/HousePage/Gallery/index.jsx
@@ -4,11 +4,28 @@ import './gallery.css'
 function Gallery({gallery}) {
     const [pictureIndex, setPictureIndex] = useState(0);
 
+    const handleNextClick = () => {
+        if (pictureIndex === gallery.length - 1) {
+          setPictureIndex(0);
+        } else {
+          setPictureIndex(pictureIndex + 1);
+        }
+      };
+    
+      const handlePreviousClick = () => {
+        if (pictureIndex === 0) {
+          setPictureIndex(gallery.length - 1);
+        } else {
+          setPictureIndex(pictureIndex - 1);
+        }
+      };
+
     return (
         <div className='house-gallery'>
-            <i class="fa-solid fa-chevron-left" onClick={() => {if (pictureIndex >0)setPictureIndex(pictureIndex-1)}}></i>
-            <img src={gallery[pictureIndex]} alt='imagem'></img>      
-            <i className="fa-solid fa-chevron-right" onClick={() => {if (pictureIndex < gallery.length -1)setPictureIndex(pictureIndex+1)}}></i>
+            {gallery.length > 1 && <i class="fa-solid fa-chevron-left previousSlide" onClick={handlePreviousClick}></i>}
+            <img src={gallery[pictureIndex]} alt='imagem'></img> 
+            <div className='picture-index'>{pictureIndex +1}/{gallery.length} </div>     
+            {gallery.length > 1 && <i className="fa-solid fa-chevron-right nextSlide" onClick={handleNextClick}></i>}
         </div>
     )
 
