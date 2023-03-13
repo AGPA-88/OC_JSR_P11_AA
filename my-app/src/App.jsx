117ab9c2c5ea24eafacc0c0822e5f5751c0039d1
Antonio Alexandre <antonio.alexandre@marte.fr>
2023-03-13 16:27:15 +0100

    Comment code removal

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,11 +1,6 @@
 // import logo from './logo.svg';
 import React from 'react';
 import './CSS/App.css';
-// import header from './App.css';
-// import Header from './Components/Header.jsx'; 
-// import Footer from './Components/Footer.jsx';
-// import Gallery from './Components/Gallery.jsx';
-// import Slogan from './Components/Slogan';
 import Homepage from './Pages/Homepage';
 
 function App() {
